79b8963bfefcc0875425f7f137f0078a3f8a70d2
Allwin Mel <allwinmel@gmail.com>
2023-11-28 12:24:59 +0530

    react-shopping-cart

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://github.com/allwinmel/react-shopping-cart"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        github.com/allwinmel/react-shopping-cart
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href="https://65658e2ccd250032dfb27333--vermillion-melba-f3c0d3.netlify.app/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Allwin Mel
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
